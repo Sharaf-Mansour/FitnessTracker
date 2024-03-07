4f3b694bef1e67fcb6490cc6edcb966d77b36b0f
Fayez Kassem <101155960+Zoz55@users.noreply.github.com>
2024-03-08 01:23:39 +0200

    FIx
    closes #4

--- FitnessInfo.cs
+++ FitnessInfo.cs
@@ -6,11 +6,10 @@ public class FitnessInfo(Gender gender = default, double age = default, double h
     public double Height { get; set; } = height;
     public double Weight { get; set; } = weight;
     public ActivityLevel ActivityLevel { get; set; } = activityLevel;
-
     public double CalculateDailyCalorieIntake() => Gender switch
     {
-        Gender.Male => (88.362 + (13.397 * weight) + (4.799 * height) - (5.677 * age)) * (double)ActivityLevel,
-        Gender.Female => (447.593 + (9.247 * weight) + (3.098 * height) - (4.330 * age)) * (double)ActivityLevel,
+        Gender.Male => (88.362 + (13.397 * Weight) + (4.799 * Height) - (5.677 * Age)) * (double)ActivityLevel,
+        Gender.Female => (447.593 + (9.247 * Weight) + (3.098 * Height) - (4.330 * Age)) * (double)ActivityLevel,
         _ => throw new ArgumentException("Invalid gender. Please specify 'Male' or 'Female'.")
     };
 }
