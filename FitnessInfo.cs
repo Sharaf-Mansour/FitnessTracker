65f2fb57239b019e2c96ccccc340977b45d62adb
Ahmed Walid <119056772+Ahmedwalid04@users.noreply.github.com>
2024-03-07 20:02:19 +0200

    Update FitnessInfo.cs
    closes #6

--- FitnessInfo.cs
+++ FitnessInfo.cs
@@ -1,11 +1,16 @@
-﻿using System;
-namespace AWFitnessTracker;
-public record FittnessInfo(string Gender, int Age, int Height, int Weight, double ActivityLevel)
+﻿namespace FitnessTracker;
+public class FitnessInfo(Gender gender = default, double age = default, double height = default, double weight = default, double activityLevel = default)
 {
-    public double CalculateDailyCalorieIntake() => Gender.ToLower() switch
+    public Gender Gender { get; set; } = gender;
+    public double Age { get; set; } = age;
+    public double Height { get; set; } = height;
+    public double Weight { get; set; } = weight;
+    public double ActivityLevel { get; set; } = activityLevel;
+
+    public double CalculateDailyCalorieIntake() => Gender switch
     {
-        "male" => (88.362 + (13.397 * Weight) + (4.799 * Height) - (5.677 * Age)) * (1 + ActivityLevel / 10),
-        "female" => (447.593 + (9.247 * Weight) + (3.098 * Height) - (4.330 * Age)) * (1 + ActivityLevel / 10),
-        _ => throw new ArgumentException("Invalid gender. Please specify 'male' or 'female'.")
+        Gender.Male => (88.362 + (13.397 * weight) + (4.799 * height) - (5.677 * age)) * (1 + activityLevel / 10),
+        Gender.Female => (447.593 + (9.247 * weight) + (3.098 * height) - (4.330 * age)) * (1 + activityLevel / 10),
+        _ => throw new ArgumentException("Invalid gender. Please specify 'Male' or 'Female'.")
     };
 }
